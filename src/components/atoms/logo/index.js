63891e4a396e76afe45b2fed1498ece45207ba59
Aline Borges <ap.borges@live.com>
2020-12-01 16:06:39 -0300

    (feat): create a component for the application logo

---
+++ src/components/atoms/logo/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './style.css'
+
+import logo from '../../../assets/images/logo.svg'
+
+const Logo = () => {
+  return(
+    <img className="logo" src={logo} alt="logo" />
+  )
+}
+
+export default Logo 
