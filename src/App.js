5b3f51e228e3a7cfae74a78092a08304b926754c
Aline Borges <ap.borges@live.com>
2020-12-01 16:14:45 -0300

    (refactor): change the structure of the App component by adding the application components

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
 import React from 'react'
 
+import './assets/styles/global-style.css'
+
+import Container from './components/atoms/container/index'
+import Header from './components/organisms/header/index'
+import Search from  './components/organisms/search/index'
+import Table from './components/organisms/table/index'
+import Product from './components/organisms/product/index'
+
+import './assets/styles/global-style.css'
+
 const App = () => {
   return (
     <>
-      <header></header>
-      <main>
-        <section>
-          <form></form>
-        </section>
-        <section>
-          <table></table>
-        </section>
-        <section>
-          <form></form>
-        </section>
-      </main>
+      <Container>
+        <Header />
+        <Search />
+        <Table />
+        <Product />
+      </Container>
     </>
   );
 }
