e4843bdc9b8ab3001b972e89a5a846d20b932eab
Aline Borges <ap.borges@live.com>
2020-12-01 16:05:40 -0300

    (feat): create a component for the menu

---
+++ src/components/molecules/menu/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './style.css'
+
+const Menu = () => {
+  return(
+    <nav>
+      <ul className="menu-list">
+        <li className="menu-item">Buscar Produto</li>
+        <li className="menu-item">Ver Lista de Produtos</li>
+        <li className="menu-item">Criar Produto</li>
+      </ul>
+    </nav>
+  )
+}
+
+export default Menu 
