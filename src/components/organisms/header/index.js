42c2a2321d2120b76dac17426367e3ea52bee4d5
Aline Borges <ap.borges@live.com>
2020-12-03 20:07:29 -0300

    (refactor): remove the component from the Header

--- src/components/organisms/header/index.js
+++ src/components/organisms/header/index.js
@@ -2,13 +2,11 @@ import React from 'react'
 import './style.css'
 
 import Logo from '../../atoms/logo/index'
-import Menu from '../../molecules/menu/index'
 
 const Header = () => {
   return(
     <header className="main-header">
       <Logo />
-      <Menu />
     </header>
   )
 }
