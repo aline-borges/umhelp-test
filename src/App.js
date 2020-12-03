edf09a2709ebe5c2206029e31a70e91185d47bfd
Aline Borges <ap.borges@live.com>
2020-12-02 23:46:14 -0300

    (feat): create the product sorting functionality of the table

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 import './assets/styles/global-style.css'
 
@@ -11,13 +11,63 @@ import Product from './components/organisms/product/index'
 import './assets/styles/global-style.css'
 
 const App = () => {
+  const [products, setProducts] = useState([])
+  const [sort, setSort] = useState({
+    key: 'id',
+    ascending: true
+  })
+
+  useEffect(() => {
+    const productsList = JSON.parse(localStorage.getItem('products')) || []
+    setProducts(productsList)
+  }, [])
+
+  const handleDelete = (id) => {
+    const filteredProducts =  products.filter((product) => product.id !== id )
+    
+    setProducts(filteredProducts)
+    localStorage.setItem('products', JSON.stringify(filteredProducts))
+  }
+
+  const handleSort = (tableField) => { 
+    if (sort.key === tableField) {
+      setSort({
+        key: tableField,
+        ascending: !sort.ascending,
+      })
+      sortProducts(tableField, !sort.ascending)
+    } else {
+      setSort({
+        key: tableField,
+        ascending: true,
+      })
+      sortProducts(tableField, true)
+    }
+  }
+
+  const sortProducts = (key, ascending) => {
+    const sortedProducts = products.sort((a, b) => {
+      if (a[key] < b[key]) {
+        return ascending ? -1 : 1
+      }
+      if (a[key] > b[key]) {
+        return ascending ? 1 : -1
+      }
+      return 0
+    })
+    setProducts(sortedProducts)
+  }
+
   return (
     <>
       <Container>
         <Header />
         <Search />
-        <Table />
         <Product />
+        <Table
+          onDelete={handleDelete}
+          onSort={handleSort}
+          products={products} />
       </Container>
     </>
   );
