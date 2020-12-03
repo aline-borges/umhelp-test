21f6f3ffd18b4f45f3818c7ea6ae29c95fd5e62c
Aline Borges <ap.borges@live.com>
2020-12-03 20:14:59 -0300

    (feat): create the function that sorts the table according to thead click and create the function responsible for adding and removing products from stock

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import React, { useState, useEffect } from 'react'
 
 import './assets/styles/global-style.css'
 
-import Container from './components/atoms/container/index'
-import Header from './components/organisms/header/index'
-import Search from  './components/organisms/search/index'
-import Table from './components/organisms/table/index'
-import Product from './components/organisms/product/index'
+import Container from './components/atoms/container'
+import Header from './components/organisms/header'
+import Table from './components/organisms/table'
+import Product from './components/organisms/product'
+import Search from './components/organisms/search'
 
 import './assets/styles/global-style.css'
 
@@ -58,15 +58,45 @@ const App = () => {
     setProducts(sortedProducts)
   }
 
+  const handleSearch = (event) => {
+    const { value } = event.target
+    const productsList = JSON.parse(localStorage.getItem('products')) || []
+    const filteredProducts = productsList.filter(product => 
+      product.productName.toLowerCase().startsWith(value.toLowerCase()))
+    setProducts(filteredProducts)
+  }
+
+  const handleStockChange = (id, operation) => {
+    const changedProduct = products.find(product => product.id === id)
+    const index = products.findIndex(product => product.id === id)
+    let newStock
+    if (operation === 'add') {
+      newStock = Number(changedProduct.stock) + 1
+    }
+    if (operation === 'sub') {
+      newStock = Number(changedProduct.stock) - 1
+    }
+    if (newStock === 0) {
+      handleDelete(id)
+      return
+    }
+    changedProduct.stock = newStock
+    const newProducts = products.filter(product => product.id !== id)
+    newProducts.splice(index, 0, changedProduct)
+    setProducts(newProducts)
+    localStorage.setItem('products', JSON.stringify(newProducts))
+  }
+
   return (
     <>
       <Container>
         <Header />
-        <Search />
         <Product />
+        <Search onSearch={handleSearch} />
         <Table
           onDelete={handleDelete}
           onSort={handleSort}
+          onChangeStock={handleStockChange}
           products={products} />
       </Container>
     </>
