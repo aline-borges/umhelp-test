e7b8456d366742686f47d222a18e3a2afa50dcfe
Aline Borges <ap.borges@live.com>
2020-12-02 23:40:48 -0300

    (feat): create product creation functionality

--- src/components/organisms/product/index.js
+++ src/components/organisms/product/index.js
@@ -1,34 +1,109 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import './style.css'
 
-import ProductInput from '../../molecules/productInput/index'
+import Input from '../../atoms/input/index'
 import Button from '../../atoms/buttons/index'
 
 import create from '../../../assets/icons/create.svg'
 
 const Product = () => {
+  const [product, setProduct] = useState({
+    id: 1,
+    productName: '',
+    price: '',
+    stock: '',
+  })
+
+  useEffect(() => {
+    const nextId = getNextId()
+    setProduct({
+      id: nextId,
+      productName: '',
+      price: '',
+      stock: '',
+    })
+  }, [])
+
+  const handleChange = (event) => {
+    const { value, name } = event.target;
+    setProduct(prevState => ({
+      ...prevState,
+      [name]: value
+    }))
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    if(product.stock === '0') {
+      alert('Estoque não pode ser abaixo de 1')
+      return
+    }
+    const products = JSON.parse(localStorage.getItem('products')) || []
+    products.push(product)
+    const orderedProducts = products.sort((a, b) => a.id - b.id)
+    localStorage.setItem('products', JSON.stringify(orderedProducts))
+    const nextId = getNextId()
+    setProduct({
+      id: nextId,
+      productName: '',
+      price: '',
+      stock: '',
+    })
+  }
+
+  const getNextId = () => {
+    const products = JSON.parse(localStorage.getItem('products')) || []
+    if (!products.length) {
+      return 1
+    }
+    const nextId = products.find((product, index) => {
+      return product.id - 1 !== index
+    })
+
+    if (nextId) return nextId.id - 1
+
+    return products.length + 1
+  }
+
   return(
     <section className="product">
-      <form className="product-form">
+      <form className="product-form" onSubmit={handleSubmit}>
         <p className="title-form">Criar Item</p>
-        <ProductInput
-        id="product-cod"
+        <Input
+        type="number"
         attribute="Cod:"
+        readOnly
+        value={product.id}
         />
-        <ProductInput 
-        id="product-name"
+
+        <Input 
+        type="text"
+        placeholder="Ex: Mesa"
         attribute="Nome:"
+        name="productName"
+        onChangeText={handleChange}
+        value={product.name}
+        required="required"
         />
-        <ProductInput
-        id="product-unit-price"
-        attribute="Preço Unitário:" />
-        <ProductInput
-        id="product-stock-quantity"
-        attribute="Em Estoque:" />
-        <ProductInput 
-        id="product-stock-price"
-        attribute="Preço do Estoque:"
+
+        <Input
+        type="number"
+        placeholder="Ex: 250.50"
+        attribute="Preço Unitário:" 
+        name="price"
+        onChangeText={handleChange}
+        value={product.price}
         />
+        
+        <Input
+        type="number"
+        placeholder="Ex: 100"
+        attribute="Em Estoque:" 
+        name="stock"
+        onChangeText={handleChange}
+        value={product.stock}
+        />
+
         <Button 
         id="create-product-button"
         text="Criar"
