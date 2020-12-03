5f1750ae7366d8f772308d5850b21f26565c20c1
Aline Borges <ap.borges@live.com>
2020-12-02 23:37:52 -0300

    (feat): add the functionality of the table that picks up the products created

--- src/components/organisms/table/index.js
+++ src/components/organisms/table/index.js
@@ -4,20 +4,81 @@ import './style.css'
 import { EditButton } from '../../atoms/buttons/index'
 
 import settings from '../../../assets/icons/settings.svg'
-import edit from '../../../assets/icons/edit.svg'
 import del from '../../../assets/icons/delete.svg'
 
 const Table = (props) => {
+  const renderRow = product => {
+    return(
+      <tr 
+      key={product.id}
+      className="products-row">
+      <td 
+      id="product-cod"
+      className="products-item">{product.id}</td>
+      <td 
+      id="product-name"
+      className="products-item">{product.productName}</td>
+      <td 
+      id="product-price"
+      className="products-item">{product.price}</td>
+      <td 
+      id="product-stock"
+      className="products-item">{product.stock}</td>
+      <td 
+      id="product-stock-price"
+      className="products-item">R${product.price * product.stock}</td>
+      <td className="products-item">
+        <EditButton
+        id="delete-button" 
+        src={del} 
+        alt="delete-icon"
+        onClick={() => props.onDelete(product.id)}
+        />
+      </td>
+    </tr>
+    )
+  }
+
   return(
     <section className="products-list">
       <table className="products-table">
         <thead className="products-header">
           <tr className="products-row">
-            <th className="products-attribute">Cod</th>
-            <th className="products-attribute">Nome</th>
-            <th className="products-attribute">Preço Unitário</th>
-            <th className="products-attribute">Em Estoque</th>
-            <th className="products-attribute">Preço do Estoque</th>
+            <th className="products-attribute">
+              <button
+                onClick={() => props.onSort('id')}
+              >
+                Cod
+              </button>
+            </th>
+            <th className="products-attribute">
+              <button
+                onClick={() => props.onSort('productName')}
+              >
+                Nome
+              </button>
+            </th>
+            <th className="products-attribute">
+              <button
+                onClick={() => props.onSort('price')}
+              >
+                Preço Unitário 
+              </button>
+            </th>
+            <th className="products-attribute">
+              <button
+                onClick={() => props.onSort('stock')}
+              >
+                Em Estoque
+              </button>              
+            </th>
+            <th className="products-attribute">
+              <button
+                onClick={() => props.onSort('stockPrice')}
+              >
+                Preço do Estoque 
+              </button>
+            </th>
             <th className="products-attribute">
               <img 
               className="settings-icon" 
@@ -28,35 +89,7 @@ const Table = (props) => {
         </thead>
 
         <tbody className="products-body">
-          <tr className="products-row">
-            <td 
-            id="product-cod"
-            className="products-item">01</td>
-            <td 
-            id="product-name"
-            className="products-item">Cadeira</td>
-            <td 
-            id="product-price"
-            className="products-item">R$ 45.00</td>
-            <td 
-            id="product-stock"
-            className="products-item">100</td>
-            <td 
-            id="product-stock-price"
-            className="products-item">R$ 450.00</td>
-            <td className="products-item">
-              <EditButton
-              id="edit-button" 
-              src={edit} 
-              alt="edit-icon"
-              />
-              <EditButton
-              id="delete-button" 
-              src={del} 
-              alt="delete-icon"
-              />
-            </td>
-          </tr>
+          {props.products.map(product => renderRow(product))}
         </tbody>
       </table>
     </section>
